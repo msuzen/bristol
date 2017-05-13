a43e7fa79b86de7a4e5413d8b28370a6a9be7da8
msuzen <mehmet.suzen@gmail.com>
2017-05-14 00:24:56 +0200

    comma in the setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ exec(fp.read())
 setup(
       name='bristol',
       version=__version__,
-      description="Bristol: A Python Package for Random Matrix Ensembles: Application of Random Matrix Theory (RMT) appear in different fields of research. Generation of random matrices numerically is an essential part of this practice. While, matrices should be generated in a numerically stable way and should represent correct matrix ensemble. Bristol implements techniques developed by Mezzadri that addresses these concerns in a Python module with parallel processing capabilities and a data model for further processing.  Circular module provides methods to generate matrices from Circular Unitary Ensemble (CUE), Circular Ortogonal Ensemble (COE) and Circular Symplectic Ensemble (CSE). Additional spectral analysis utilities are also implemented, such as computation of spectral density and spectral ergodicity."
+      description="Bristol implements techniques developed by Mezzadri with parallel processing capabilities and a data model for further processing for generating random matrices. Circular module provides methods to generate matrices from Circular Unitary Ensemble (CUE), Circular Ortogonal Ensemble (COE) and Circular Symplectic Ensemble (CSE). Additional spectral analysis utilities are also implemented, such as computation of spectral density and spectral ergodicity.",
       url="https://github.com/msuzen/bristol",
       author='M.Suzen, C.Garbers',
       author_email='suzen@acm.org',
