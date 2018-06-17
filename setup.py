3a425fcf26d555ee6f6013f6d99b3a531b18280b
msuzen <msuzen@gmail.com>
2018-06-17 19:08:41 +0200

    long description in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
       name='bristol',
       version=__version__,
       description="Bristol implements techniques developed by Mezzadri with parallel processing capabilities and a data model for further processing for generating random matrices. Circular module provides methods to generate matrices from Circular Unitary Ensemble (CUE), Circular Ortogonal Ensemble (COE) and Circular Symplectic Ensemble (CSE). Additional spectral analysis utilities are also implemented, such as computation of spectral density and spectral ergodicity.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/msuzen/bristol",
       author='M.Suzen',
       author_email='suzen@acm.org',
